697168a6a84f38e5d4e12b02e95df1ade23ae20d
Владислав Пономаренко <nightnomad@GECK.local>
2017-01-29 22:22:39 +0300

    initial commit: passes 5 tests of 14

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,13 +1,35 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
+    }
 
-    append(data) {}
+    append(data) {
+        var node = new Node(data);
 
-    head() {}
+        if (this.length) {
+            this._tail.next = node;
+            node.previous = this.tail;
+            this._tail = node;
+        } else {
+            this._head = node;
+            this._tail = node;
+        };
 
-    tail() {}
+        this.length++;
+        return node;
+    }
+
+    head() {
+      return this._head.data;
+    }
+
+    tail() {
+      return this._tail.data;
+    }
 
     at(index) {}
 
